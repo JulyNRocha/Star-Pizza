0bdda027b34f8345fa040bbfbf5521c1b39179ad
July Rocha <nrochajuly@gmail.com>
2022-11-04 17:20:41 -0300

    :sparkles: Add Explosion VFX to enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+
     void OnParticleCollision(GameObject other)
     {
+        Instantiate(deathVFX,transform.position, Quaternion.identity);
+       
         Destroy(gameObject);
     }
 }
