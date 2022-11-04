f093553a0fced8a7706f0a5ebbb681fbf0107e71
July Rocha <nrochajuly@gmail.com>
2022-11-04 19:37:41 -0300

    :sparkles: Add Score points system script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,8 +8,26 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
 
+    [SerializeField] int scorePerHit = 15;
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
     void OnParticleCollision(GameObject other)
     {
+        ProcessHit();
+        StartDeathSequence();
+    }
+
+    void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void StartDeathSequence(){
         GameObject vfx = Instantiate(deathVFX,transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
