94d17993e59698a23f506eed2f46503b0d9cdcd8
July Rocha <nrochajuly@gmail.com>
2022-11-05 02:04:23 -0300

    :sparkles: Import Enemies space ships assets and make preFabs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,30 +6,45 @@ public class Enemy : MonoBehaviour
 {
 
     [SerializeField] GameObject deathVFX;
-    [SerializeField] Transform parent;
+    [SerializeField] int hitPoints = 3;
+    [SerializeField] GameObject hitVFX;
+     [SerializeField] int scorePerHit = 15;
 
-    [SerializeField] int scorePerHit = 15;
+   
     ScoreBoard scoreBoard;
+    GameObject parentGameObject;
 
     void Start()
     {
+        AddRigidbody();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRunTime");
         scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    void AddRigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     void OnParticleCollision(GameObject other)
     {
+        if (hitPoints < 1) { StartDeathSequence(); }     
         ProcessHit();
-        StartDeathSequence();
     }
 
     void ProcessHit()
     {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parentGameObject.transform;
         scoreBoard.IncreaseScore(scorePerHit);
+        hitPoints--;
+
     }
 
     void StartDeathSequence(){
         GameObject vfx = Instantiate(deathVFX,transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
     }
 }
